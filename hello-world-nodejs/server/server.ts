35709b8f6df4d71429a070652fb5c43730a8d670
Leonardo Moya <88905913+lsmcba@users.noreply.github.com>
2022-09-06 23:47:34 -0300

    Update server.ts

--- hello-world-nodejs/server/server.ts
+++ hello-world-nodejs/server/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 // Create a new express app instance
+
+const PORT = process.env.PORT || 3000;
 const app: express.Application = express();
 app.get("/hello", function (req, res) {
   res.send("Hello World!");
