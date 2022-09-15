653b263b54b2ccb0b6fee2f75fbf1a433069d754
Leonardo Moya <lsmcba@gmail.com>
2022-09-14 21:10:04 -0400

    ADD: Subiendo Imagenes

---
+++ prueba.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl http://localhost/health,
+curl http://localhost/nodejs/hello,
+curl http://localhost/golang/hello,
+curl http://localhost/golang/get-scores?name"Leonardo",
+curl http://localhost/golang/inc-score
